92d7d64c03402545103ce3ce4b3a6d30c11d6c7b
Satriaariq23 <136548620+Satriaariq23@users.noreply.github.com>
2023-06-20 22:23:12 +0700

    Update Savemoney algoritma.py

--- Savemoney algoritma.py
+++ Savemoney algoritma.py
@@ -9,12 +9,15 @@ class Keuangan:
     def tambah_pengeluaran(self, pengeluaran):
         self.data_pengeluaran.append(pengeluaran)
 
-    def bubble_sort(self, data):
-        n = len(data)
-        for i in range(n - 1):
-            for j in range(n - i - 1):
-                if data[j] > data[j + 1]:
-                    data[j], data[j + 1] = data[j + 1], data[j]
+    def bubble_sort(arr):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    print(income_list)
+    print(expense_list)
 
     def urutkan_keuangan(self):
         self.bubble_sort(self.data_pemasukan)
